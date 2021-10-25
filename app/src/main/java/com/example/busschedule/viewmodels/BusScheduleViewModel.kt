34e28205f8d9c240b251b80a4f6c8ab80071202b
Brian <byamagata@gmail.com>
2021-10-24 17:30:25 -0700

    Create ListAdapter.  Implement Flow for responding to data changes.  Fix bug in BusScheduleViewModel class.

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -4,19 +4,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
-import java.lang.IllegalArgumentException
+import kotlinx.coroutines.flow.Flow
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
-    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
 
-    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 }
 
 class BusScheduleViewModelFactory(
     private val scheduleDao: ScheduleDao
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(BusScheduleViewModelFactory::class.java)) {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return BusScheduleViewModel(scheduleDao) as T
         }
